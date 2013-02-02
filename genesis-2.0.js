92a208f106bb9263474cecd158b2d1bc2ccaf25d
GaryJones <gary@garyjones.co.uk>
2013-02-02 15:29:14 +0000

    Initial commit.

---
+++ genesis-2.0.js
@@ -0,0 +1,18 @@
+/**
+ * @externs
+ */
+
+/** @type {Object} */
+var genesisL10n;
+
+/**
+ * @type {string}
+ * @const
+ */
+genesisL10n.category_checklist_toggle;
+
+/**
+ * @type {string}
+ * @const
+ */
+genesisL10n.saveAlert;
---
+++ prose-2.0.js
@@ -0,0 +1,132 @@
+/**
+ * @externs
+ */
+
+/** @type {Object} */
+var proseL10n;
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.first_time;
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.save_url;
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.tab;
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.subtab;
+
+/**
+ * @type {number}
+ * @const
+ */
+proseL10n.post_vars;
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.helper;
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.toggle_all;
+
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.settings_saved;
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.warn_unsaved;
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.warn_unsaved_one;
+
+/**
+ * @type {string}
+ * @const
+ */
+proseL10n.warn_reset;
+
+jQuery.disableSelection = function() {};
+
+// ajaxManager plugin
+jQuery.manageAjax;
+
+ //UI Namespace
+jQuery.ui;
+
+/**
+ * @type {Element}
+ * @const
+ */
+jQuery.ui.panel;
+
+/**
+ * @type {string}
+ * @const
+ */
+jQuery.ui.panel.id;
+
+jQuery.prototype.tabs = function() {};
+
+/**
+ * @param {Object|string} arg
+ */
+jQuery.prototype.sortable = function(arg) {};
+
+/**
+ * @param {?jQuery} element
+ */
+jQuery.prototype.farbtastic = function(element) {};
+
+/**
+ * @type {string}
+ * @const
+ */
+var pagenow;
+
+/** @type {Object} */
+var postboxes;
+
+/**
+ * @nosideeffects
+ * @param {Array|jQuery|null|number|string} box
+ */
+postboxes.pbshow = function( box ){};
+
+/**
+ * @nosideeffects
+ * @param {Array|jQuery|null|number|string} box
+ */
+postboxes.pbhide = function( box ){};
+
+/**
+ * @nosideeffects
+ * @param {string} pagenow
+ */
+postboxes.add_postbox_toggles = function( pagenow ){};
